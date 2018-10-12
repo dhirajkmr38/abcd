8d211d64fb83b229b0357e26a77db36c4c8bdf11
dhiraj <dhiraj@infrrd.ai>
2018-10-12 23:09:27 +0530

    Rotten Orange added

---
+++ myCode/src/main/java/com/practice/RottenOrange.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class RottenOrange
+{
+    public static void main( String args[] )
+    {
+        System.out.println( "Hello" );
+
+        for ( int i = 0; i < 10; i++ ) {
+            System.out.println( i );
+        }
+
+    }
+}
